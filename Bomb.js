f4979c4da57f9c44f358bce80d002820022eb58c
Allison Schulman <68669789+sneakin-allie@users.noreply.github.com>
2020-12-19 13:19:21 -0500

    Revised HelloWorld, Bomb & RouletteGun

--- Bomb.js
+++ Bomb.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class Bomb extends Component {
-  state = {
-    count: 0,
-  };
+class Bomb extends React.Component {
+    state = {
+        count: 0
+    };
 
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({
-        count: this.state.count + 1
-      })
-    }, 1000)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-
-  renderDisplay() {
-    const { count } = this.state
-    if (count >= 8) {
-      clearInterval(this.interval)
-      return 'BOOM!!!!'
-    } else if (count % 2 === 0) {
-      return 'tick'
-    } else {
-      return 'tock'
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({
+                count: this.state.count +1
+            })
+        }, 1000)
     }
-  }
 
-  render() {
-    return (
-      <div className='CountdownBomb'>
-        {this.renderDisplay()}
-      </div>
-    )
-  }
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    renderDisplay() {
+        const {count} = this.state
+        if (count >= 8) {
+            clearInterval(this.interval)
+            return 'BOOM!!!!'
+        } else if (count % 2 === 0) {
+            return 'tick'
+        } else {
+            return 'tock'
+        }
+    }
+
+    render() {
+        return (
+            <div className='CountdownBomb'>
+                <p>{this.renderDisplay()}</p>
+            </div>
+        )
+    }
 }
+
+export default Bomb; 
--- HelloWorld.js
+++ HelloWorld.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react'
+import react from "react";
+import React from 'react';
 
-export default class HelloWorld extends Component {
+class HelloWorld extends React.Component {
     state = {
         who: 'world'
     };
 
     render() {
         return (
-            <div className='HelloWorld'>
-                <p>Hello, {this.state.who}!</p>
-                <button onClick={() => this.setState({ who: 'world'})}>World</button>
+            <div className="HelloWorld">
+                <p>Hello {this.state.who}!</p>
+                <button onClick={() => this.setState({who: 'world'})}>World</button>
                 <br />
-                <button onClick={() => this.setState({ who: 'friend'})}>Friend</button>
+                <button onClick={() => this.setState({who: "friend"})}>Friend</button>
                 <br />
-                <button onClick={() => this.setState({ who: 'React'})}>React</button>
+                <button onClick={() => this.setState({who: "React"})}>React</button>
             </div>
         )
     }
 }
+
+export default HelloWorld;
